0f5a15077c2068146316d55d3dc5008f8843a2d1
brooklyndadcore <scott.waring@gmail.com>
2018-07-29 15:05:43 -0400

    Update introduction.rb

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,20 @@
 Failures:
  
   1) #introduction takes in an argument of a name and puts out a phrase with that name using string interpolation
+def introduction(name)
+ puts "Hi, my name is #{name}."
+end
+
+introduction("Josh")
+
+def introduction_with_langauge(name, language)
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_langauge("Scott", "Ruby")
+
+
+
      Failure/Error: expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
      NoMethodError:
        undefined method `introduction' for #<RSpec::ExampleGroups::Introduction:0x007fdbc499a938>
